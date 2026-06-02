fb5d5f99522037d9236c606b02adffa9e3c050d3
Shashank <rokshashank@gmail.com>
2026-06-02 21:17:43 +0530

    login page

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
+from flask_login import (
+    LoginManager,
+    login_user,
+    logout_user,
+    login_required,
+    current_user
+)
 from flask import Flask, render_template, request, redirect, flash
 from werkzeug.security import generate_password_hash
 from flask import Flask
 from config import Config
 from models import db
-
+from werkzeug.security import check_password_hash
 app = Flask(__name__)
-
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
 app.config.from_object(Config)
-
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 db.init_app(app)
 
 with app.app_context():
@@ -15,7 +26,7 @@ with app.app_context():
 
 @app.route("/")
 def home():
-    return "Server Active"
+    return render_template("home.html")
 from models import db, User
 
 @app.route("/register", methods=["GET", "POST"])
@@ -46,5 +57,42 @@ def register():
         return redirect("/")
 
     return render_template("register.html")
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+
+        email = request.form["email"]
+        password = request.form["password"]
+
+        user = User.query.filter_by(email=email).first()
+
+        if user and check_password_hash(
+            user.password,
+            password
+        ):
+
+            login_user(user)
+
+            return redirect("/dashboard")
+
+        return "Invalid Email or Password"
+
+    return render_template("login.html")
+@app.route("/dashboard")
+@login_required
+def dashboard():
+
+    return render_template(
+        "dashboard.html",
+        user=current_user
+    )
+@app.route("/logout")
+@login_required
+def logout():
+
+    logout_user()
+
+    return redirect("/")
 if __name__ == "__main__":
     app.run(debug=True)
