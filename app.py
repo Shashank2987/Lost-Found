e6a3c3d3a34fb4d4fd619bc67ae1cf1126990546
Shashank <rokshashank@gmail.com>
2026-06-02 20:17:56 +0530

    registration page

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+from flask import Flask, render_template, request, redirect, flash
+from werkzeug.security import generate_password_hash
 from flask import Flask
-
 from config import Config
 from models import db
 
@@ -15,6 +16,35 @@ with app.app_context():
 @app.route("/")
 def home():
     return "Server Active"
+from models import db, User
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+
+    if request.method == "POST":
+
+        name = request.form["name"]
+        email = request.form["email"]
+        password = request.form["password"]
+
+        existing_user = User.query.filter_by(email=email).first()
+
+        if existing_user:
+            return "Email already registered"
+
+        hashed_password = generate_password_hash(password)
+
+        new_user = User(
+            name=name,
+            email=email,
+            password=hashed_password
+        )
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect("/")
+
+    return render_template("register.html")
 if __name__ == "__main__":
     app.run(debug=True)
